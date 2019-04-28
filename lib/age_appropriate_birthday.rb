98c20a507c79adb55181d0e05d2a87e93be4f0c5
Fabian Fabro <fabrofabian@gmail.com>
2019-04-28 01:49:28 +0000

    Automatically backed up by Learn

---
+++ lib/age_appropriate_birthday.rb
@@ -0,0 +1,6 @@
+def age_appropriate_birthday(birthday_kids)
+  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
